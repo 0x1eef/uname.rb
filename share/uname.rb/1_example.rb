0d03c5fe96e263c79245b9298dee5675c5249ebe
0x1eef <0x1eef@protonmail.com>
2023-03-22 02:54:03 -0300

    Update example

--- share/uname.rb/1_example.rb
+++ share/uname.rb/1_example.rb
@@ -1,16 +1,18 @@
+require_relative "setup"
 require "uname"
+
 name = Kernel.uname
-puts name.class
-puts name.sysname
-puts name.nodename
-puts name.release
-puts name.version
-puts name.machine
+print "class = ", name.class, "\n"
+print "sysname = ", name.sysname, "\n"
+print "nodename = ", name.nodename, "\n"
+print "release =", name.release, "\n"
+print "version = ", name.version, "\n"
+print "machine = ", name.machine, "\n"
 
 ##
-# UNIXName
-# FreeBSD
-# 0.al-quran.reflectslight.local
-# 14.0-CURRENT-HBSD
-# FreeBSD 14.0-CURRENT-HBSD #0  hardened/current/master-n192545-c7d6d4bb487: ...
-# amd64
+# class = UNIXName
+# sysname = FreeBSD
+# nodename = 0.al-quran.reflectslight.local
+# release = 14.0-CURRENT-HBSaD
+# version = FreeBSD 14.0-CURRENT-HBSD #0  hardened/current/master-n192545-c7d6d4bb487: ...
+# machine = amd64
